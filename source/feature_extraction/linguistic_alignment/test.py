e4a74905ad44c74f5602a828630744959078d723
David Semke <david@semke.ca>
2023-05-15 01:23:28 -0700

    Added demonstration of extraction and idea_flows functions

--- source/feature_extraction/linguistic_alignment/test.py
+++ source/feature_extraction/linguistic_alignment/test.py
@@ -1,31 +1,21 @@
-from convokit import Corpus, Utterance, Speaker
+from extraction import avg_idea_participation_percentage, median_idea_discussion_time
 from idea_flow import idea_flows
 from source.constants import gap_corpus
 
+
 convo = gap_corpus.get_conversation('1.Pink.1')
-
-# person = Speaker(id="0")
-
-# utts = [
-#     Utterance(id="0", conversation_id="0", text="Manchester United are looking to sign a larger forward for $90 million", speaker=person),
-# ]
-
-
-# corpus = Corpus(utterances=utts)
-# convo = corpus.get_conversation('0')
-
-flows = idea_flows(convo, gap_corpus)
+idea_flows_dict = idea_flows(convo, gap_corpus)
 
 print()
-print(f"Total adjective idea flows: {len(flows['J'])}")
-print(f"Total noun idea flows: {len(flows['N'])}")
-print(f"Total verb idea flows: {len(flows['V'])}")
+for key in idea_flows_dict:
+    for flow in idea_flows_dict[key]:
+        print(flow['tok'])
+        for id in flow['utt_ids']:
+            print(f"\t{id}")
 
-for key in flows:
-    for flow in flows[key]:
-        print(f"First {key} idea flow: {flow}")
-        break
-
+print()
+print('Median idea discussion time:', median_idea_discussion_time(idea_flows_dict), 'secs')
+print('Average idea participation percentage:', avg_idea_participation_percentage(convo, idea_flows_dict), '%')
 print()
 
 
