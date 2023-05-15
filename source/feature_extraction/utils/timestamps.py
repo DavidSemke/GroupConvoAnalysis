1cc018e3512af33bd31762dab6f3c81425e911e7
David Semke <david@semke.ca>
2023-05-15 04:33:31 -0700

    Added utils function that allows conversion from secs to timestamp

--- source/feature_extraction/utils/timestamps.py
+++ source/feature_extraction/utils/timestamps.py
@@ -4,10 +4,8 @@ def convert_to_secs(timestamp):
     return int(mins) * 60 + float(secs)
 
 
-def add_timestamps(t1, t2):
-    t1_secs = convert_to_secs(t1)
-    t2_secs = convert_to_secs(t2)
-    secs = round(t1_secs + t2_secs, 1)
+def convert_to_timestamp(secs):
+    secs = round(float(secs), 1)
     split = str(secs).split('.')
     secs, decisecs = [int(s) for s in split]
     mins = min(secs // 60, 99)
@@ -15,3 +13,8 @@ def add_timestamps(t1, t2):
     return f"{mins:02d}:{leftover_secs:02d}.{decisecs}"
 
 
+def add_timestamps(t1, t2):
+    t1_secs = convert_to_secs(t1)
+    t2_secs = convert_to_secs(t2)
+    secs = t1_secs + t2_secs
+    return convert_to_timestamp(secs)
