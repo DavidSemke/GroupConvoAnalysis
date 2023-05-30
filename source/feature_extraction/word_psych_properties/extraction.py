a817cdeb31ca0c01ceb6245cfcf5241e99f0c1d2
David Semke <david@semke.ca>
2023-05-30 16:33:01 -0700

    Placed extraction helper function into psych_property_scores.py

--- source/feature_extraction/word_psych_properties/extraction.py
+++ source/feature_extraction/word_psych_properties/extraction.py
@@ -1,8 +1,4 @@
-from convokit import TextParser
-from nltk.stem import WordNetLemmatizer
 import numpy as np
-from source.feature_extraction.word_psych_properties.mrc_psych_db import query_mrc_db, avg_ratings
-from source.feature_extraction.utils.content_token import is_content_word, lemmatize_content_word
 from source.feature_extraction.utils.stats import variance
 
 """
@@ -21,49 +17,3 @@ def psych_property_score_variances(ratings_matrix):
         vars.append(round(variance(ratings_matrix[:, i], 2)))
     
     return vars
-
-
-# uses speaker vocabulary to calculate scores
-def speaker_psych_property_scores(speaker, convo, corpus):
-    # filter out utterances not included in convo/do not belong to speaker
-    corpus = corpus.filter_utterances_by(lambda u: u.conversation_id == convo.id and u.speaker.id == speaker.id)
-
-    # define parser and parse corpus
-    parser = TextParser()
-    corpus = parser.transform(corpus)
-
-    # initialize lemmatizer
-    lemmatizer = WordNetLemmatizer()
-
-    words = []
-    for utt in corpus.iter_utterances():
-        
-        first_word_not_found = True
-        for tok_dict in [tok_dict for parsed_dict in utt.meta['parsed'] 
-                         for tok_dict in parsed_dict['toks']]:
-            
-            tok = tok_dict['tok']
-
-            if not tok.isalnum(): continue
-
-            # exclude proper nouns
-            is_proper_noun = tok_dict['tag'][0:3] == 'NNP'
-
-            if is_proper_noun: 
-                first_word_not_found = False
-                continue
-
-            if first_word_not_found:
-                first_word_not_found = False
-                is_content = is_content_word(tok_dict, parser, True)
-            else:
-                is_content = is_content_word(tok_dict, parser, False)
-            
-            if is_content:
-                tok = lemmatize_content_word(tok_dict, lemmatizer)
-            
-            words.append(tok)
-    
-    ratings = query_mrc_db(words)
-
-    return avg_ratings(ratings)
---
+++ source/feature_extraction/word_psych_properties/psych_property_scores.py
@@ -0,0 +1,49 @@
+from convokit import TextParser
+from nltk.stem import WordNetLemmatizer
+from source.feature_extraction.word_psych_properties.mrc_psych_db import query_mrc_db, avg_ratings
+from source.feature_extraction.utils.content_token import is_content_word, lemmatize_content_word
+
+# uses speaker vocabulary to calculate scores
+def speaker_psych_property_scores(speaker, convo, corpus):
+    # filter out utterances not included in convo and do not belong to speaker
+    corpus = corpus.filter_utterances_by(lambda u: u.conversation_id == convo.id and u.speaker.id == speaker.id)
+
+    # define parser and parse corpus
+    parser = TextParser()
+    corpus = parser.transform(corpus)
+
+    # initialize lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    words = []
+    for utt in corpus.iter_utterances():
+        
+        first_word_not_found = True
+        for tok_dict in [tok_dict for parsed_dict in utt.meta['parsed'] 
+                         for tok_dict in parsed_dict['toks']]:
+            
+            tok = tok_dict['tok']
+
+            if not tok.isalnum(): continue
+
+            # exclude proper nouns
+            is_proper_noun = tok_dict['tag'][0:3] == 'NNP'
+
+            if is_proper_noun: 
+                first_word_not_found = False
+                continue
+
+            if first_word_not_found:
+                first_word_not_found = False
+                is_content = is_content_word(tok_dict, parser, True)
+            else:
+                is_content = is_content_word(tok_dict, parser, False)
+            
+            if is_content:
+                tok = lemmatize_content_word(tok_dict, lemmatizer)
+            
+            words.append(tok)
+    
+    ratings = query_mrc_db(words)
+
+    return avg_ratings(ratings)
