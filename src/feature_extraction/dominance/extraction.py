2b145e0c5d883d9277fbd59eef8fdcfb78ba1c2d
David Semke <david@semke.ca>
2023-07-25 23:08:58 -0700

    Added RQA speech overlap extraction funcs

--- src/feature_extraction/dominance/extraction.py
+++ src/feature_extraction/dominance/extraction.py
@@ -1,34 +1,44 @@
 import numpy as np
+from src.recurrence.rqa.extraction import (
+    epoch_rqa_lam, 
+    epochless_rqa_stats
+)
 from src.recurrence.rqa.feature_rqa import (
     simult_binary_speech_sampling_rqa
 )
 
-# Returns the RQA trial that produces the max mean in determinism
-# using both early and late frame epochs
-def simult_binary_speech_sampling_frame_det(convo):
-    trials = simult_binary_speech_sampling_rqa(convo, 'frame')
-    l = lambda t: (
-        np.mean([epoch.determinism for epoch in t['results']])
+# Returns the max mean for frame epoch laminarity and the trial that 
+# achieved the max mean
+def speech_overlap_frame_lam(convo):
+    return epoch_rqa_lam(
+        simult_binary_speech_sampling_rqa(convo, 'frame')
     )
-    best_trial = max(trials, key=l)
-    det_mean = l(best_trial)
-
-    return det_mean, best_trial
 
 
-# Returns the max aggregate score for determinism diffs between 
-# epochs, where epochs are adjacent such that they span the entire 
-# time series
+# Returns the max aggregate score for laminarity diffs between 
+# epochs (where epochs are adjacent such that they span the entire 
+# time series) and the trial that achieved the score
 # The aggregate function takes a list of numbers and outputs a number
-def simult_binary_speech_sampling_sliding_det(
-        convo, aggregate_func=np.mean
-):
-    trials = simult_binary_speech_sampling_rqa(convo, 'sliding')
-    l = lambda t: (
-        aggregate_func([epoch.determinism for epoch in t['results']]
+# Default aggregate function takes the mean of differences between
+# adjacent epochs (late epoch lam - early epoch lam)
+def speech_overlap_sliding_lam(convo):
+    return epoch_rqa_lam(
+        simult_binary_speech_sampling_rqa(convo, 'sliding'), 
+        lambda lams: np.mean(np.diff(lams))
+    )
+
+
+# Returns avg and longest vertical line len for trial with greatest
+# laminarity (avg vertical line len = trapping time)
+# Uses RQA without epochs (to avoid interrupting vertical lines)
+def speech_overlap_vertical_stats(convo):
+    return epochless_rqa_stats(
+        simult_binary_speech_sampling_rqa(convo),
+        lambda e: {
+            'trapping_time': e.trapping_time,
+            'longest_vertical_line': e.longest_vertical_line
+        },
+        lambda trials: max(
+            trials, key=lambda trial: trial['results'][0].laminarity
         )
     )
-    best_trial = max(trials, key=l)
-    aggregate = l(best_trial)
-
-    return aggregate, best_trial
